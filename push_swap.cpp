31a7fc94fd04cd3e8a7ca71b2e3a57822859a501
김인회 <90288195+inhoekim@users.noreply.github.com>
2023-05-24 07:16:06 +0900

    Update push_swap.cpp

--- push_swap.cpp
+++ push_swap.cpp
@@ -92,6 +92,9 @@ void partition(int from, int size, int *ps, char type) {
 	int pivot2 = arr[(size / 3) * 2];
 	ps[0] = size / 3; ps[1] = size / 3; ps[2] = size / 3 + size % 3;
 	deque<int>::iterator iter;
+	int remain_s = ps[0];
+	int remain_m = ps[1];
+	int remain_b = ps[2];
 	//A에서 파티션이 일어난 경우
 	if (from == A) {
 		for (int i = 0; i < size; i++) {
@@ -99,39 +102,45 @@ void partition(int from, int size, int *ps, char type) {
 			if (dqs[A].front() < pivot) {
 				PP(A, B);
 				RO(B);
+				remain_s--;
 				//중간영역(M)
 			} else if (pivot <= dqs[A].front() && dqs[A].front() < pivot2) {
 				PP(A, B);
+				remain_m--;
 				//큰영역(L)
-			} else if (dqs[A].front() >= pivot2) {
+			} else if (dqs[A].front() >= pivot2 && remain_s != 0 && remain_m != 0) {
 				RO(A);
+				remain_b--;
 			}
 		}
-		//RRO를 해서 파티셔닝한 L을 위로 올리는게 나은지, RO를 해서 기존 덱영역을 밑으로 내리는게 나은지 확인(최적화 과정)
-		if (dqs[A].size() - ps[2] > ps[2])
-			for (int j = 0; j < ps[2]; j++)
+		//RRO를 해서 파티셔닝한 L을 위로 올리는게 나은지, RO를 해서 고정영역을 밑으로 내리는게 나은지 확인(최적화 과정)
+		if (2 * (ps[2] - remain_b) < dqs[A].size())
+			for (int j = 0; j < ps[2] - remain_b; j++)
 				RRO(A);
 		else
-			for (int j = 0; j < dqs[A].size() - ps[2]; j++)
+			for (int j = 0; j < dqs[A].size() - ps[2] + remain_b; j++)
 				RO(A);
 	}
 		//B에서 파티션이 일어난 경우
 	else {
 		for (int i = 0; i < size; i++) {
-			if (dqs[B].front() < pivot) {
+			if (dqs[B].front() < pivot && remain_m != 0 && remain_b != 0) {
 				RO(B);
+				remain_s--;
 			} else if (pivot <= dqs[B].front() && dqs[B].front() < pivot2) {
 				PP(B, A);
 				RO(A);
+				remain_m--;
 			} else if (dqs[B].front() >= pivot2) {
 				PP(B, A);
+				remain_b--;
 			}
 		}
-		if (dqs[B].size() - ps[0] > ps[0])
-			for (int j = 0; j < ps[0]; j++)
+		if (2 * (ps[0] - remain_s) < dqs[B].size())
+			for (int j = 0; j < ps[0] - remain_s; j++)
 				RRO(B);
 		else
-			for (int j = 0; j < dqs[B].size() - ps[2]; j++)
+			for (int j = 0; j < dqs[B].size() - ps[2] + remain_s; j++)
 				RO(B);
 	}
 }
@@ -186,7 +195,6 @@ int main(void) {
 	}
 	order(A, n, 'L');
 	cout << cnt << "\n" << ss.str();
-	/*
 	deque<int>::iterator iter;
 	int n = 0;
 	for (iter = dqs[A].begin(); iter != dqs[A].end(); iter++) {
@@ -198,5 +206,4 @@ int main(void) {
 		n++;
 	}
 	cout << cnt << endl;
-	*/
 }
